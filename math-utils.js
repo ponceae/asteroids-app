c13ee7539c8e67b93b9b43666902c66671804afe
Adrien P <adrienponce7@gmail.com>
2026-06-02 15:47:10 -0400

    Formatted Vector2.js and added a utility helper module for converting degrees to radians.

---
+++ math-utils.js
@@ -0,0 +1,19 @@
+/**
+ * Math Utilities Module.
+ * 
+ * Contains helper functions for vector math.
+ * 
+ * @module math-utils
+ * @author Adrien P.
+ */
+
+/**
+ * Convert the given degrees to radians.
+ * 
+ * @param {number} degrees - The number to convert. 
+ * @returns {number} The converted number.
+ */
+function degreesToRadians(degrees)
+{
+  return degrees * (Math.PI / 180);
+}
